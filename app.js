7103a2224bfd5759770e4d2e1f0c46bb873798cb
GeneralLeander <mafanga@gmail.com>
2016-05-09 10:39:31 +0200

    created app for kasi_artisans

---
+++ app.js
@@ -0,0 +1,43 @@
+const express      = require('express'),
+      exhbs        = require('express-handlebars'),
+      cookieParser = require('cookie-parser'),
+      session      = require('express-session'),
+      bodyParser   = require('body-parser'),
+      //mysql        = require('mysql'),
+      connectionPv = require('connection-provider'),
+      compression  = require('compression'),
+      app          = express();
+
+const dbOptions = {
+  host      : 'localhost',
+  user      : 'root',
+  password  : '2197832',
+  port      : 3306,
+  database  : 'artisans'
+};
+
+
+
+//app.use(connectionPv(dbOptions, serviceSetupCallBack));
+app.use(cookieParser('shhhh, very secret'));
+app.use(session({ secret : 'keyboard cat', cookie :{ maxAge : 3600000 }, resave : true, saveUninitialized : true }));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
+app.use(compression());
+//app.use(flash());
+app.engine('handlebars', exhbs({defaultLayout : 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get("/", function(req, res) {
+  res.render("/index");
+});
+
+
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('App running on http://%s:%s', host, port);
+});
