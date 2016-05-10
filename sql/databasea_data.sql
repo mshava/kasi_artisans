3a9d2faabd4a81b33682ef612305f12b801910bf
Mshava <sakhile@projectcodex.co>
2016-05-10 10:51:20 +0200

    table artisans populated

---
+++ sql/databasea_data.sql
@@ -0,0 +1,9 @@
+  insert into artisans (id, name, contacts, address, type) values(1, 'Siya', '086 182 8002', '103 king Ave Parrow', 'machanic');
+
+  insert into artisans (id, name, contacts, address, type) values(2, 'Ayabonga', '083 786 5412', '13 Malview Section', 'plumber');
+
+  insert into artisans (id, name, contacts, address, type) values(3, 'Akhona', '061 062 7723', '103 Tabalaza Street', 'electrician');
+
+  insert into artisans (id, name, contacts, address, type) values(4, 'Ziyanda', '073 458 6621', '889 Blackburn Ave', 'hairdresser');
+
+  insert into artisans (id, name, contacts, address, type) values(5, 'Siya', '079 465 8072', '02 king Ave Parrow', 'capentaty');
