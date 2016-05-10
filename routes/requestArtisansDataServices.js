662451cdf9a73d04efd31603e0fccdd9f9df0b50
Mshava <sakhile@projectcodex.co>
2016-05-10 11:17:05 +0200

    fixed syntax error

--- routes/requestArtisansDataServices.js
+++ routes/requestArtisansDataServices.js
@@ -2,11 +2,12 @@ const QueryService = require('../data_services/query-service');
 const Promise = require('bluebird');
 const showArtisansDataServices = require('../data_services/showArtisansDataServices');
 
-module.exports = function() {
+module.exports = function(connection) {
     const queryService = new QueryService(connection);
     const sdql = new showArtisansDataServices(connection);
 
     this.showArtisans = function(id) {
       return QueryService.executeQuery('select * from artisans where id = ?', id);
     };
+
 };
