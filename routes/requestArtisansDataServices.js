35d5d43797ee57d30cd377dd587121f0abaae4d6
Mshava <sakhile@projectcodex.co>
2016-05-10 11:13:24 +0200

    request artisans data services created

---
+++ routes/requestArtisansDataServices.js
@@ -0,0 +1,12 @@
+const QueryService = require('../data_services/query-service');
+const Promise = require('bluebird');
+const showArtisansDataServices = require('../data_services/showArtisansDataServices');
+
+module.exports = function() {
+    const queryService = new QueryService(connection);
+    const sdql = new showArtisansDataServices(connection);
+
+    this.showArtisans = function(id) {
+      return QueryService.executeQuery('select * from artisans where id = ?', id);
+    };
+};
