1ee889f7b942340155abe1fe42a619867944e9a0
Mshava <sakhile@projectcodex.co>
2016-05-10 12:01:37 +0200

    function for showing artisans created

---
+++ routes/showArtisans.js
@@ -0,0 +1,17 @@
+exports.show = function(req, res, next) {
+  req.getServices()
+    .then(function(services) {
+      const showArtisansDataServices = services.showArtisansDataServices;
+      console.log(services);
+      const id = req.params.id;
+        showArtisansDataServices.showArtisans()
+          .then(function(artisans) {
+            res.render('show-artisans', {
+              artisans : artisans
+            });
+          });
+    });
+    .catch(function(error){
+      next(error);
+    });
+};
